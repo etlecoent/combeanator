663095780e4e9a46c75bae27ee2314f524583d68
Étienne Le Coënt <etlecoent@gmail.com>
2026-03-06 14:42:24 -0500

    feat(backend): implement the processing of gold coffees

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,6 +11,7 @@ import logger from './shared/logger.js';
 import errorMiddleware from './shared/middlewares/errorMiddleware.js';
 import loggerMiddleware from './shared/middlewares/loggerMiddleware.js';
 import notFoundMiddleWare from './shared/middlewares/notFoundMiddleware.js';
+import { startCoffeesIngestion } from './workers/coffeesIngestion.js';
 
 const app = express();
 
@@ -36,20 +37,35 @@ const server = app.listen(ENV.PORT, () => {
 	logger.info(`Server running on port ${ENV.PORT}`);
 });
 
+// Start Coffees Ingestion
+const { close } = await startCoffeesIngestion();
+
 const shutdown = async (signal: string) => {
 	logger.info(`${signal} signal received: closing HTTP server`);
 
 	server.close(async () => {
 		logger.info('HTTP server closed');
 
+		logger.info('Closing workers');
+		try {
+			await close();
+			logger.info('Workers closed');
+		} catch (err) {
+			logger.error(err, 'Error closing workers');
+			process.exit(1);
+		}
+
+		logger.info('Closing database connection');
 		try {
 			await db.destroy();
 			logger.info('Database connection closed');
-			process.exit(0);
 		} catch (err) {
 			logger.error(err, 'Error closing database connection');
 			process.exit(1);
 		}
+
+		logger.info('Shutdown complete');
+		process.exit(0);
 	});
 
 	// Force shutdown after 60 seconds
