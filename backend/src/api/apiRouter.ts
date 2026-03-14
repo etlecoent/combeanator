3cdab51bda05556055745f34b179aec7b7e56321
Étienne Le Coënt <etlecoent@gmail.com>
2026-03-14 16:25:47 -0400

    feat: add roasters route to API router

--- backend/src/api/apiRouter.ts
+++ backend/src/api/apiRouter.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import coffeesRouter from './coffees/coffeesRouter.js';
 import healthRouter from './health/healthRouter.js';
+import roastersRouter from './roasters/roastersRouter.js';
 import usersRouter from './users/usersRouter.js';
 
 const apiRouter: Router = Router();
@@ -9,5 +10,6 @@ const apiRouter: Router = Router();
 apiRouter.use('/health', healthRouter);
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/coffees', coffeesRouter);
+apiRouter.use('/roasters', roastersRouter);
 
 export default apiRouter;
